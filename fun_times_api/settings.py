30fb2202a03c3d6dc4c4c9937a605b189cb6a57b
Nazek-Altayeb <>
2023-09-04 21:28:59 +0000

    clean code in setting.py

--- fun_times_api/settings.py
+++ fun_times_api/settings.py
@@ -158,7 +158,6 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-    print('connected')
 
 # DATABASES = {
 #    'default': {
@@ -202,7 +201,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
