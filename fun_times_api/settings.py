f80e4995f6d7b403096cc80b4ad70ec92b88c4f3
Nazek-Altayeb <>
2023-09-06 14:50:18 +0000

    comment out the CORS condition

--- fun_times_api/settings.py
+++ fun_times_api/settings.py
@@ -110,14 +110,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-if 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-else:
-    CORS_ALLOWED_ORIGIN_REGEXES = [
-        "^https://.*\.codeanywhere\.io$",
-    ]
+# if 'CLIENT_ORIGIN' in os.environ:
+#    CORS_ALLOWED_ORIGINS = [
+#        os.environ.get('CLIENT_ORIGIN')
+#    ]
+#else:
+#    CORS_ALLOWED_ORIGIN_REGEXES = [
+#        "^https://.*\.codeanywhere\.io$",
+#    ]
+
 CORS_ALLOW_CREDENTIALS = True
 
 ROOT_URLCONF = 'fun_times_api.urls'
