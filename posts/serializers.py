9488e8ac5d70a19b711e06e53d70be287dcac327
Nazek-Altayeb <>
2023-10-04 08:26:23 +0000

    add the fields (adventure_id, adventures_count to the post serializers)

--- posts/serializers.py
+++ posts/serializers.py
@@ -13,6 +13,8 @@ class PostSerializer(serializers.ModelSerializer):
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
     comments_count = serializers.ReadOnlyField()
+    adventure_id = serializers.ReadOnlyField()
+    adventures_count = serializers.ReadOnlyField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -53,5 +55,6 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image',
-            'like_id', 'likes_count', 'comments_count', 'visibility', 'followers'
+            'like_id', 'likes_count', 'comments_count', 'visibility', 
+            'followers', 'adventure_id', 'adventures_count'
         ]
